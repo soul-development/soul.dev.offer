4c90d1d18f8d05c916c6c98f8e0229225cab4f8c
bmspd <my_spid@mail.ru>
2025-06-11 21:18:16 +0200

    feat(github actions): add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://soul-development.github.io/',
   base: '/soul.dev.offer/',
-  output: 'static'
 });
