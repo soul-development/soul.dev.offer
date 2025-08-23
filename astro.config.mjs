3ee0b24227c05626b2658c9767ac1a11d608ba5a
bmspd <my_spid@mail.ru>
2025-08-23 23:52:53 +0500

    feat(scripts): add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://souldev.site',
+  integrations: [
+    partytown({ config: { forward: ['dataLayer.push'] } }),
+  ],
 });
